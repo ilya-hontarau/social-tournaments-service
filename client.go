1a690bf446caa64c7feb7b7c10ae3c9ed4d410ab
ilya <illfate254@gmail.com>
2019-05-31 17:33:37 +0300

    Fix returning errors, refactor main function

--- client.go
+++ client.go
@@ -20,43 +20,39 @@ type User struct {
 	Hobby string `json:"hobby"`
 }
 
-const (
-	URL      = "http://localhost:9000"
-	URLPARAM = "http://localhost:9000?id="
-)
+const baseURL = "http://localhost:9000"
 
 func main() {
 	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
-		fmt.Printf("usage: %s ", filepath.Base(os.Args[0]))
+		fmt.Printf("usage: %s\n-g <name> <your hobby>\n-p <id>\n", filepath.Base(os.Args[0]))
 		os.Exit(1)
 	}
 
 	if os.Args[1] == "-p" && len(os.Args) > 3 {
 		result, err := postUser(os.Args[2], os.Args[3:])
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("could not add user, ", err)
 		}
 		fmt.Println(result)
 	} else if os.Args[1] == "-g" && len(os.Args) == 3 {
 		result, err := getUser(os.Args[2])
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("could not get user, ", err)
 		}
 		fmt.Println(result)
 	} else {
-		fmt.Printf("incorrect request, usage: %s\n", filepath.Base(os.Args[0]))
+		log.Printf("incorrect request, usage: %s\n", filepath.Base(os.Args[0]))
 	}
 }
 
 func postUser(name string, hobby []string) (string, error) {
-	var user User
-	user.Name, user.Hobby = name, strings.Join(hobby, " ")
+	user := User{name, strings.Join(hobby, " ")}
 	userData, err := json.Marshal(user)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
-	resp, err := http.Post(URL, "application/json", bytes.NewBuffer(userData))
+	resp, err := http.Post(baseURL, "application/json", bytes.NewBuffer(userData))
 	if err != nil {
 		return "", err
 	}
@@ -74,7 +70,7 @@ func getUser(id string) (string, error) {
 	if _, err := strconv.Atoi(id); err != nil {
 		return "", errors.New("incorrect id")
 	}
-	resp, err := http.Get(URLPARAM + os.Args[2])
+	resp, err := http.Get(fmt.Sprintf("%s?id=%s", baseURL, id))
 	if err != nil {
 		return "", err
 	}
@@ -83,7 +79,10 @@ func getUser(id string) (string, error) {
 	var user User
 	err = json.NewDecoder(resp.Body).Decode(&user)
 	if err != nil {
-		return "", errors.New("id doesn't exist")
+		if resp.StatusCode == http.StatusNotFound {
+			return "", errors.New("id doesn't exist")
+		}
+		return "", err
 	}
 	return fmt.Sprintf("%s,your hobby is %s", user.Name, user.Hobby), nil
 }
