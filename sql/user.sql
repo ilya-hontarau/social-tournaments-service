d1140a1614d823427e258fab499608b341155bd3
ilya <illfate254@gmail.com>
2019-06-03 16:57:21 +0300

    Rename db, table column name

--- sql/user.sql
+++ sql/user.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE user_db;
-USE user_db;
+CREATE DATABASE tournament_db;
+USE tournament_db;
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT,
-    user_name VARCHAR(20) NOT NULL,
+    name VARCHAR(20) NOT NULL,
     balance INT(10) UNSIGNED NOT NULL,
     PRIMARY KEY (ID)
 );
