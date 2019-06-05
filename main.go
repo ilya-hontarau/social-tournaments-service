b20b702f68a2d6484ef56714318210b72d43cdcf
ilya <illfate254@gmail.com>
2019-06-05 16:53:42 +0300

    Add server constructor, edit error handling

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -13,7 +14,6 @@ import (
 )
 
 //TODO: create a function that parse URL and switch according to URl
-//TODO: create a Server constructor
 
 // User represents a single user that is registered in a social tournaments service.
 type User struct {
@@ -22,106 +22,119 @@ type User struct {
 	Balance uint   `json:"balance"`
 }
 
-// Server represents server with pointer to a DB.
+const userEnvVar = "DB_USER"
+const passEnvVar = "DB_PASS"
+const dbNameEnvVar = "DB_NAME"
+
+// Server represents а db server in social.
 type Server struct {
 	DB *sql.DB
 }
 
-func dbConn() (db *sql.DB, err error) {
-	dbDriver := "mysql"
-	dbUser := "root"
-	dbPass := "Sql_DB_tournament1"
-	dbName := "tournament_db"
-
-	db, err = sql.Open(dbDriver, fmt.Sprintf("%s:%s@/%s", dbUser, dbPass, dbName))
+// NewServer constructs a Server, according to existing env variables
+func NewServer() (*Server, error) {
+	dbUser := os.Getenv(userEnvVar)
+	if dbUser == "" {
+		return nil, fmt.Errorf(`no "%s" env variable`, userEnvVar)
+	}
+	dbPass := os.Getenv(passEnvVar)
+	dbName := os.Getenv(dbNameEnvVar)
+	if dbName == "" {
+		return nil, fmt.Errorf(`no "%s" env variable`, dbNameEnvVar)
+	}
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/%s", dbUser, dbPass, dbName))
 	if err != nil {
 		return nil, fmt.Errorf("can't open db: %s", err)
 	}
-	return db, nil
+	if err := db.Ping(); err != nil {
+		if dbPass == "" {
+			return nil, fmt.Errorf(`no "%s" env variable: %s`, passEnvVar, err)
+		}
+		return nil, err
+	}
+	return &Server{db}, nil
 }
 
 func main() {
-	db, err := dbConn()
+	s, err := NewServer()
 	if err != nil {
 		log.Print(err)
 		return
 	}
-	defer db.Close()
-	s := Server{
-		DB: db,
-	}
+	defer s.DB.Close()
+
 	http.HandleFunc("/user", s.addUser)
 	http.HandleFunc("/user/", s.getUser)
-	err = http.ListenAndServe("localhost:9000", nil)
+	err = http.ListenAndServe("localhost:9100", nil)
 	if err != nil {
 		log.Print(err)
 		return
 	}
 }
 
-func (s *Server) addUser(writer http.ResponseWriter, req *http.Request) {
+func (s *Server) addUser(w http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
-		http.NotFound(writer, req)
+		http.NotFound(w, req)
 		return
 	}
 	var user User
 	err := json.NewDecoder(req.Body).Decode(&user)
 	if err != nil {
-		writer.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(writer, "cannot decode json: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "cannot decode json: %s", err)
 		return
 	}
 	insert, err := s.DB.Exec("INSERT INTO user(name,balance) VALUES(?,?)", user.Name, 0)
 	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(writer, "could not add user: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "could not add user: %s", err)
 		return
 	}
 	user.ID, err = insert.LastInsertId()
 	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	err = json.NewEncoder(writer).Encode(struct {
+	err = json.NewEncoder(w).Encode(struct {
 		ID int64 `json:"id"`
 	}{
 		ID: user.ID,
 	})
 	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(writer, "can't encode json: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "can't encode json: %s\n", err)
 		return
 	}
 
 }
 
-func (s *Server) getUser(writer http.ResponseWriter, req *http.Request) {
+func (s *Server) getUser(w http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodGet {
-		http.NotFound(writer, req)
+		http.NotFound(w, req)
 		return
 	}
 	idIndex := strings.LastIndex(req.URL.Path, "/") // idIndex can't be -1
 	id, err := strconv.Atoi(req.URL.Path[idIndex+1:])
 	if err != nil {
-		writer.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(writer, "incorrect id: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "incorrect id: %s", err)
 		return
 	}
 	var user User
 	err = s.DB.QueryRow("SELECT id, name,balance FROM user WHERE id = ?", id).
 		Scan(&user.ID, &user.Name, &user.Balance)
 	if err == sql.ErrNoRows {
-		writer.WriteHeader(http.StatusNotFound)
+		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	err = json.NewEncoder(writer).Encode(user)
+	err = json.NewEncoder(w).Encode(user)
 	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(writer, "can't encode json: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "can't encode json: %s\n", err)
 		return
 	}
 }
