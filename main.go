cf457377fceb2fd1568be0c4afec90985bb0884f
ilya <illfate254@gmail.com>
2019-06-06 17:05:28 +0300

    Fix data race, refactor functions
    Delete enum. Add additional error checking.

--- main.go
+++ main.go
@@ -13,8 +13,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-//TODO: create a function that parse URL and switch according to URl
-
 // User represents a single user that is registered in a social tournaments service.
 type User struct {
 	ID      int64  `json:"id"`
@@ -28,18 +26,9 @@ const (
 	dbNameEnvVar = "DB_NAME"
 )
 
-type message int
-
-const (
-	def message = iota
-	take
-	fund
-)
-
 // Server represents а db server in social tournaments service.
 type Server struct {
-	DB  *sql.DB
-	msg message
+	DB *sql.DB
 }
 
 // NewServer constructs a Server, according to existing env variables.
@@ -70,7 +59,7 @@ func main() {
 
 	http.HandleFunc("/user", s.addUser)
 	http.HandleFunc("/user/", s.handler)
-	err = http.ListenAndServe("localhost:9000", nil)
+	err = http.ListenAndServe(":9000", nil)
 	if err != nil {
 		log.Print(err)
 		return
@@ -114,19 +103,15 @@ func (s *Server) addUser(w http.ResponseWriter, req *http.Request) {
 }
 
 func (s *Server) handler(w http.ResponseWriter, req *http.Request) {
-	index := strings.LastIndex(req.URL.Path, "/") // idIndex can't be -1
-	word := req.URL.Path[index+1:]
-	switch word {
-	case "take":
-		s.msg = take
-	case "fund":
-		s.msg = fund
-	default:
+	switch strings.Count(req.URL.Path, "/") { // count slash
+	case 3:
+		s.processBonus(w, req)
+	case 2:
 		s.getUser(w, req)
+	default:
+		http.NotFound(w, req)
 		return
 	}
-	s.processBonus(w, req)
-
 }
 
 func (s *Server) processBonus(w http.ResponseWriter, req *http.Request) {
@@ -151,18 +136,36 @@ func (s *Server) processBonus(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "cannot decode json: %s", err)
 		return
 	}
-	switch s.msg {
-	case fund:
-		_, err = s.DB.Exec("UPDATE user SET balance = balance + ? WHERE id = ?", bonus.Points, id)
-	case take:
-		_, err = s.DB.Exec("UPDATE user SET balance = balance - ? WHERE id = ?", bonus.Points, id)
+	var update sql.Result
+
+	switch strings.Split(req.URL.Path, "/")[3] {
+	case "fund":
+		update, err = s.DB.Exec(`
+UPDATE user 
+   SET balance = balance + ? 
+ WHERE id = ?`, bonus.Points, id)
+	case "take":
+		update, err = s.DB.Exec(`
+UPDATE user 
+   SET balance = balance - ? 
+ WHERE id = ?`, bonus.Points, id) // add additional check
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "could not update balance: %s", err)
+		return
+	}
+	rows, err := update.RowsAffected()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if rows == 0 {
+		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 }
@@ -180,7 +183,10 @@ func (s *Server) getUser(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	var user User
-	err = s.DB.QueryRow("SELECT id, name,balance FROM user WHERE id = ?", id).
+	err = s.DB.QueryRow(`
+SELECT id, name,balance 
+  FROM user 
+ WHERE id = ?`, id).
 		Scan(&user.ID, &user.Name, &user.Balance)
 	if err == sql.ErrNoRows {
 		w.WriteHeader(http.StatusNotFound)
