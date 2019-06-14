40f6dec6b3a56ad56a36b65af08ce621dd30ef53
ilya <illfate254@gmail.com>
2019-06-14 11:42:46 +0300

    Add tournament sql-script, edit user script

---
+++ sql/tournament.sql
@@ -0,0 +1,18 @@
+ CREATE TABLE tournaments (
+     id INT NOT NULL AUTO_INCREMENT,
+     name VARCHAR(20) NOT NULL,
+     deposit INT(10) UNSIGNED NOT NULL,
+     prize INT(10) UNSIGNED NOT NULL,
+     finished BOOL DEFAULT false,
+     winner INT,/*add for key*/
+     FOREIGN KEY(winner) REFERENCES users(id),
+     PRIMARY KEY(id)
+);
+ 
+CREATE TABLE participants (
+    user_id INT NOT NULL,
+    tournament_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (tournament_id) REFERENCES tournaments(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id,tournament_id)
+);
